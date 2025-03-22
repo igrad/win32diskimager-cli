d667c53fbe705d6edea8a4fd28b3c4681530fff2
Ian Gradert <iangradert@gmail.com>
2025-03-21 20:14:42 -0500

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -18,27 +18,58 @@
  **********************************************************************/
 
 #ifndef WINVER
-#define WINVER 0x0601
+#   define WINVER 0x0601
 #endif
+
+#include "mainwindow.h"
 
 #include <QApplication>
 #include <cstdlib>
+#include <cxxopts.hpp>
 #include <windows.h>
 #include <winioctl.h>
-#include "mainwindow.h"
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    app.setApplicationDisplayName(VER);
-    app.setAttribute(Qt::AA_UseDesktopOpenGL);
+   cxxopts::Options options("win32diskimager",
+      "A Windows tool for writing images to USB sticks or SD/CF cards.");
 
-    QTranslator translator;
-    if (translator.load("translations/diskimager_" + QLocale::system().name()))
-        app.installTranslator(&translator);
+   options.add_options()
+      ("i,image", "The image file to read into/write from")
+      ("v,volume", "The volume to read from/write to")
+      ("d,drive", "Same as -v")
+      ("s,skip-confirmation", "Skip all confirmation prompts")
+      ("x,hash", "Hash algorthm to use. If -f no hash algorithm is specified, SHA256 will be used. Options are MD5, SHA1, and SHA256.")
+      ("f,write-hash-to-file", "The generated has will be written to this file")
+      ("w,write", "Write image file to volume")
+      ("r,read", "Read image from volume to file")
+      ("a,read-only-allocated-partitions", "Ready only from allocated partiftions on the volume")
+      ("V,verify-only", "Only verify the image")
+      ("o,write-out", "Write all output from this command run to a specified file. If -q is not specified, output will still print to the shell.")
+      ("q,quiet", "Hush all output from this command. If -o is specified in tandem, output will only be put to the output file.")
+      ("verbose", "Verbose output (mostly for debugging)", cxxopts::value<bool>()->default_value("false"));
 
-    MainWindow* mainwindow = MainWindow::getInstance();
-    mainwindow->show();
-    return app.exec();
+   auto args = options.parse(argc, argv);
+   const bool headlessMode = argc > 1;
+
+   if(!headlessMode)
+   {
+      QApplication app(argc, argv);
+      app.setApplicationDisplayName(VER);
+      app.setAttribute(Qt::AA_UseDesktopOpenGL);
+
+      QTranslator translator;
+      if(translator.load("translations/diskimager_" + QLocale::system().name()))
+         app.installTranslator(&translator);
+
+      MainWindow* mainwindow = MainWindow::getInstance();
+      mainwindow->show();
+
+      return app.exec();
+   }
+   else
+   {
+      return 0;
+   }
 }
